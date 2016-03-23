79d609c38108fa7bfc87f86b31c49d3e3f551f5f
Francid <splinzy@yahoo.com>
2016-03-23 10:01:59 -0400

    Update Scene and Added enemy movement control script

---
+++ Assets/_Scripts/WallNailController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallNailController : MonoBehaviour {
+
+	//PUBLIC VARIABLES
+	public float speed;
+	public float zMin;
+	public float zMax;
+	public int switchDirection;
+
+	//PRIVATE VARIABLES
+	private Transform _transform;
+	private Vector3 _currentPosition;
+
+
+	// Use this for initialization
+	void Start () {
+
+		this._transform = gameObject.GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		this._currentPosition = this._transform.position;
+
+		if (this._currentPosition.z >= this.zMin) {
+			this.switchDirection = 0;
+		}
+
+		if (this._currentPosition.z <= this.zMax) {
+			this.switchDirection = 1;
+		}
+
+		if (this.switchDirection == 0) {
+			this._currentPosition -= new Vector3 (0f,0f, speed);
+			this._transform.position = this._currentPosition;
+		}
+		if (this.switchDirection == 1) {
+			this._currentPosition += new Vector3 (0f,0f, speed);
+			this._transform.position = this._currentPosition;
+		}
+
+	}
+		
+}
